061945ed3a0d8cb61e63b36db3db5fc7026b9807
ibobai <eaboabaida@gmail.com>
2022-02-10 09:37:14 +0100

    Console logging json data

--- js/jsonLocalManipulation.js
+++ js/jsonLocalManipulation.js
@@ -1,12 +1,20 @@
 
 // To read json files in javascript
-const fs = require('fs');
-let rawdata = fs.readFileSync("/home/ibobai/Simplon/Projets/Tp/tpJson/tp1.json");
-let student = JSON.parse(rawdata);
-console.log(student);
+// const fs = require('fs');
+// let rawdata = fs.readFileSync("/home/ibobai/Simplon/Projets/Tp/tpJson/tp1.json");
+// let student = JSON.parse(rawdata);
+// console.log(student);
 
 
-const file = require("file");
-let data = file.readFileSync("/home/ibobai/Simplon/Projets/Tp/tpJson/tp1.json");
-let dataList = JSON.parse(data);
-console.log(dataList);
+// const file = require("file");
+// let data = file.readFileSync("/home/ibobai/Simplon/Projets/Tp/tpJson/tp1.json");
+// let dataList = JSON.parse(data);
+// console.log(dataList);
+
+
+const fs = require("fs");
+
+let ss = fs.readFileSync("/home/ibobai/Simplon/Projets/Tp/tpJson/tp1.json");
+let s2 = JSON.parse(ss);
+
+console.log(s2.squadName);
